af7c08cbfce9ca5d183be4c0542b17c843fbbea4
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-08-20 00:07:11 +0530

    explored container and text

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,36 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text("Hello World"),
-    ),
-    body: Container(
-      child: Center(child: Text("yo boi")),
-    ));
+          title: Text("Hello World"),
+        ),
+        body: Center(
+            child: Container(
+          // color: Colors.cyan,
+          width: 200,
+          height: 100,
+          padding: const EdgeInsets.all(20),
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            "hello there I am Hayat",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+              fontSize: 20,
+            ),
+          ),
+          decoration: BoxDecoration(
+              color: Colors.lightGreen,
+              gradient:
+                  LinearGradient(colors: [Colors.red, Colors.lightBlue[900]]),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[350],
+                  blurRadius: 10,
+                  offset: Offset(5, 5),
+                )
+              ]),
+        )));
   }
 }
