baaaa05b80c32d14065d94e4b9000b57dfddd8e7
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-08-20 19:00:12 +0530

    explored drawer and listview

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ void main() {
   runApp(MaterialApp(
     title: "my App",
     home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.orange,
+    ),
   ));
 }
 
@@ -12,46 +15,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Hello World"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: Colors.red,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.cyan[200],
-                  width: 50,
-                  height: 100,
-                  padding: const EdgeInsets.all(20),
-                ),
-                Container(
-                  color: Colors.cyan[400],
-                  width: 50,
-                  height: 100,
-                  padding: const EdgeInsets.all(20),
-                ),
-                Container(
-                  color: Colors.cyan[600],
-                  width: 50,
-                  height: 100,
-                  padding: const EdgeInsets.all(20),
-                ),
-                Container(
-                  color: Colors.cyan[800],
-                  width: 50,
-                  height: 100,
-                  padding: const EdgeInsets.all(20),
-                )
-              ],
+      appBar: AppBar(
+        title: Text("Hello World"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: Container(),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            // DrawerHeader(
+            //     child: Text("Headspace",
+            //         style: TextStyle(
+            //           fontSize: 24,
+            //         )),
+            //     decoration: BoxDecoration(
+            //         color: Colors.deepOrangeAccent[100],
+            //         borderRadius: BorderRadius.only(
+            //             bottomRight: Radius.circular(10),
+            //             bottomLeft: Radius.circular(10)))),
+            UserAccountsDrawerHeader(
+              accountName: Text("Hayat"),
+              accountEmail: Text("hayat.tamboli@gmail.com"),
+              // currentAccountPicture: Image.network(
+              //     "https://assets.codepen.io/3996536/internal/avatars/users/default.png?format=auto&height=512&version=1578931840&width=512"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://assets.codepen.io/3996536/internal/avatars/users/default.png?format=auto&height=512&version=1578931840&width=512"),
+              ),
             ),
-          ),
-        ));
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text("Hayat"),
+              subtitle: Text("your account"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.add_circle),
+              title: Text("Add"),
+              subtitle: Text("Add your post"),
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text("Notifications"),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+    );
   }
 }
